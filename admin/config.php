24f044aa47d8f5e89be6dc971981d66bba632f9b
abdulwahabahmad96 <abdul.wahab96@hotmail.com>
2019-05-09 14:36:43 +0500

    set connection string for live server

--- admin/config.php
+++ admin/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host 	= "localhost";
-$dbName = "wtd";
-$user 	= "root";
-$pass 	= "";
+// $host 	= "localhost";
+// $dbName = "wtd";
+// $user 	= "root";
+// $pass 	= "";
 
 // $host 	= "192.168.10.7:8080";
 // $dbName = "wtd";
@@ -11,10 +11,10 @@ $pass 	= "";
 // $pass 	= "";
 
 
-// $host 	= "localhost";
-// $dbName = "wtdusao2_wtdusa";
-// $user 	= "wtdusao2_wtdusa";
-// $pass 	= "WWaTSeSaO]d9";
+$host 	= "localhost";
+$dbName = "wtdusao2_wtdusa";
+$user 	= "wtdusao2_wtdusa";
+$pass 	= "WWaTSeSaO]d9";
 
 $db = new mysqli($host,$user,$pass,$dbName);
 session_start();
--- config.php
+++ config.php
@@ -4,15 +4,15 @@
 // $user 	= "root";
 // $pass 	= "";
 
-$host 	= "localhost";
-$dbName = "wtd";
-$user 	= "root";
-$pass 	= "";
-
 // $host 	= "localhost";
-// $dbName = "wtdusao2_wtdusa";
-// $user 	= "wtdusao2_wtdusa";
-// $pass 	= "WWaTSeSaO]d9";
+// $dbName = "wtd";
+// $user 	= "root";
+// $pass 	= "";
+
+$host 	= "localhost";
+$dbName = "wtdusao2_wtdusa";
+$user 	= "wtdusao2_wtdusa";
+$pass 	= "WWaTSeSaO]d9";
 
 $db = new mysqli($host,$user,$pass,$dbName);
 ?>

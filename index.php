a45b7a3e518239975456a84c66d7414958e9c0c2
abdulwahabahmad96 <abdul.wahab96@hotmail.com>
2019-05-13 18:58:56 +0500

    index update

--- index.php
+++ index.php
@@ -1005,7 +1005,7 @@ if (isset($_GET['error'])) {
                         </div>
                     </div> 
                     <div data-v-55126885 class=title-content-container>
-                        <h5 data-v-55126885 class=title>WTD Warehouse Extension Comming Soon</h5> <h5 data-v-55126885 class=content>WTD Warehouse Extension</h5> 
+                        <h5 data-v-55126885 class=title>WTD Warehouse Expension Coming Soon</h5> <h5 data-v-55126885 class=content>WTD Warehouse Extension</h5> 
                         <div data-v-55126885 class=button-container>
                             <div data-v-49614bf8 data-v-55126885 class="component-ArrowLink small">
                                 <div data-v-49614bf8 class=link>

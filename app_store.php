07c18bc44cb870f0b6f3a9c3cd7d0ae1dfc71850
abdulwahabahmad96 <abdul.wahab96@hotmail.com>
2019-05-13 17:48:23 +0500

    index page changes

---
+++ app_store.php
@@ -0,0 +1,15 @@
+<div data-v-0d6be6a8 data-v-5ff8ac3c id=service-offering class=component-ServicesOffering style=background-image:url(images/action.jpg)>
+     <div data-v-0d6be6a8 class=common-center-container align="center">
+         <h2 data-v-0d6be6a8 class="heading webkit-font">Find Us On Apple Store & Play Store As WTDUSA</h2>
+         <div data-v-0d6be6a8 class=cards-container>
+            <div class="row" style="margin-top: 10%;">
+                <div class="col-md-6" align="right">
+                    <img src="images/Google_play.png" style="width: 71%;padding-top: 14px;">
+                </div>
+                <div class="col-md-6" align="left">
+                    <img src="images/AppStore.png" style="width: 70%;">
+                </div>
+            </div> 
+        </div>
+    </div>
+</div> 

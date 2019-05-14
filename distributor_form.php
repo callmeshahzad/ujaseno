5edc8e1a27d4b0511105bbc258ab24c684905776
Abdullah <bhatti9t7@gmail.com>
2019-05-14 12:34:56 +0500

    change captcha code in distributer form

--- distributor_form.php
+++ distributor_form.php
@@ -41,33 +41,34 @@
 }
 .captcha-input {
 	background: url('captcha_code.php') repeat-y;
-	padding-left: 85px;	
+	padding-left: 85px;
 	padding-top: 2px;
 	background-color: white;
     color: black;
 }
 </style>
+<script src="js/distributerform.js"></script>
 <div data-v-68d9be01 data-v-5ff8ac3c id=form-section class=component-FormSection style="!important; padding:20px; background:url(images/back.png)">
  	<div data-v-68d9be01 class=common-center-container style="-webkit-box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75);
 -moz-box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75);
 box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75)">
  		<h3 data-v-68d9be01 class="heading webkit-font">FILL THE DEALERSHIP  FORM TO GET THE WTD ADVANTAGE</h3>
  		<h4 data-v-68d9be01 class="subheading webkit-font">Let’s grow your business together. Provide your details, and an WTD associate will contact you.</h4>
- 		<?php 
-        if ($msg) {
-            if ($msg == 'success') {
-                echo '<div class="alert alert-success">
+ 		<?php
+if ($msg) {
+    if ($msg == 'success') {
+        echo '<div class="alert alert-success">
                       <strong>Success!</strong> WTD coustomer service repesentative will contact you shorty
                     </div>';
-            }else{
-                echo '<div class="alert alert-danger">
+    } else {
+        echo '<div class="alert alert-danger">
                       <strong>Oooops!</strong> Application Not send Please try again.
                     </div>';
-            }
-        }
-        ?>
-        <div class="row">
-            <form id=contact-form action="submitform.php" method="post" style="color: #ffffff;">
+    }
+}
+?>
+        <div class="row" >
+            <form id=contact-form action="submitform.php" method="post" style="color: #ffffff;" name="distributerForm" onsubmit="return validateCaptcha();">
                 <div class="col-sm-6">
                     <div class="form-group">
                         <label>Name</label>
@@ -110,61 +111,76 @@ box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75)">
                             <option>Other</option>
                         </select>
                     </div>
-					
                 </div>
-                <div class="row">
-                    <div class="col-sm-6 center-block" style="float: none;">
-                        <div class="form-group">
+                <div class="col-sm-12 row">
+                    <div class="col-sm-3 center-block" style="float: none;">
+                        <!-- <div class="form-group">
                             <label> Not A Roboat: <div id="error-captcha" class="demo-error"><?php if (isset($_GET['error_message'])) {echo "Incorrect Captcha Code";}?></div></label>
                             <input name="captcha_code" type="text" class="demo-input captcha-input">
+                        </div> -->
+                        <div class="form-group">
+                        
+                            <label>Not a robot</label>
+                            <div id="captcha">
+                            </div>
+
+                             <div class="input-group">
+                             <input type="text" placeholder="Captcha" id="cpatchaTextBox" class="form-control"/>
+                                <div class="input-group-btn">
+                                <button class="btn btn-default" type="button" onclick="createCaptcha()">
+                                    <i class="glyphicon glyphicon-refresh"></i>
+                                </button>
+                                </div>
+                            </div>
                         </div>
                         <div class="form-group" align="center">
-                            <button type="submit" class="btn btn-primary" style="bottom: 15px;width: 150px;background: red;border: red; font-weight:bold">SUBMIT</button>
+                            <button type="submit" class="btn btn-primary" style="bottom: 15px;width: 100%;background: red;border: red; font-weight:bold">SUBMIT</button>
                         </div>
                     </div>
                 </div>
+                
             </form>
         </div>
  		<!--<form data-v-68d9be01 id=contact-form action="submitform.php" method="post">
  			<div data-v-68d9be01 class=form-container>
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=text id=Name name=name class=input required value> 
+ 					<input data-v-68d9be01 type=text id=Name name=name class=input required value>
  					<label data-v-68d9be01 for=Name>Name</label>
- 				</div> 
- 				
+ 				</div>
+
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=text id=City name="businessname" class=input required value> 
+ 					<input data-v-68d9be01 type=text id=City name="businessname" class=input required value>
  					<label data-v-68d9be01 for=City>Business Name</label>
- 				</div> 
+ 				</div>
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=email id=Email name=email class="input email" required value> 
+ 					<input data-v-68d9be01 type=email id=Email name=email class="input email" required value>
  					<label data-v-68d9be01 for=Email>Email</label>
- 				</div> 
+ 				</div>
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=text id=license name=license class="input" value> 
+ 					<input data-v-68d9be01 type=text id=license name=license class="input" value>
  					<label data-v-68d9be01 for=Email>State License No.</label>
- 				</div> 
- 				
+ 				</div>
+
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=tel id=Phone name=phone class=input required value> 
+ 					<input data-v-68d9be01 type=tel id=Phone name=phone class=input required value>
  					<label data-v-68d9be01 for=Phone>Phone</label>
  				</div>
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=text id=City name=city class=input value> 
+ 					<input data-v-68d9be01 type=text id=City name=city class=input value>
  					<label data-v-68d9be01 for=City>City</label>
- 				</div> 
+ 				</div>
  				<div data-v-68d9be01 class=input-wrap>
- 					<input data-v-68d9be01 type=text id=State name=state class=input value> 
+ 					<input data-v-68d9be01 type=text id=State name=state class=input value>
  					<label data-v-68d9be01 for=State>State</label>
- 				</div>  
+ 				</div>
  				<div data-v-68d9be01 class=form-dropdown>
  					<select data-v-68d9be01 size=1 form=car-form class=dropdown-options name="question">
- 						<option data-v-68d9be01 disabled selected value class=option>What are you interested in</option> 
+ 						<option data-v-68d9be01 disabled selected value class=option>What are you interested in</option>
  						<option data-v-68d9be01 class=option>Partner with WTD</option>
  						<option data-v-68d9be01 class=option>Learn more about Tire Pros</option>
  						<option data-v-68d9be01 class=option>Learn more about TireBuyer</option>
  						<option data-v-68d9be01 class=option>Other</option>
- 					</select> 
+ 					</select>
 				 	<div data-v-68d9be01 class=placeholder-mimic>
 				 		What are you interested in
 				 		<div data-v-68d9be01 class=triangle>
@@ -174,13 +190,13 @@ box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75)">
 				 		</div>
 				 	</div>
  				</div>
-			</div> 
-			 <div data-v-68d9be01 class=button-container> 
+			</div>
+			 <div data-v-68d9be01 class=button-container>
 			 	<div data-v-36eb5109 data-v-68d9be01 class="component-Button webkit-font">
 				 	<button data-v-36eb5109 type=submit class="button-container 2">
 				 		<div data-v-36eb5109 class=label>
 					 		Submit
-					 	</div> 
+					 	</div>
 					 	<div data-v-36eb5109 class=arrow>
 					 		<svg viewBox="0 0 90 90">
 					 			<path fill=#0015f9 fill-rule=evenodd d="M90 45L0 90V0z"></path>
@@ -191,4 +207,5 @@ box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75)">
 			</div>
 		</form>-->
 	</div>
-</div> 
+</div>
+

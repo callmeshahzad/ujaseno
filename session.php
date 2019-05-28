7a9f97a376c212f61b6952e59e7a8e06b7d9b9c1
Abdullah <bhatti9t7@gmail.com>
2019-05-28 14:33:10 +0500

    add image and try to fix session

--- session.php
+++ session.php
@@ -1,7 +1,9 @@
 <?php 
 ob_start();
-ini_set('session.cookie_lifetime', 60 * 60 * 24 * 100);
-ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 100);
-session_start();
+// ini_set('session.cookie_lifetime', 60 * 60 * 24 * 100);
+// ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 100);
+session_start([
+    'cookie_lifetime' => 86400,
+]);
 include 'config.php';
 ?>

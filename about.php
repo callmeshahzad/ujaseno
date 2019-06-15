d138876c9afff918b56524b222892d953db5b30e
Abdullah <bhatti9t7@gmail.com>
2019-06-15 20:18:02 +0500

    add style

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 <link rel=icon type=image/png sizes=32x32 href="images/1.png">
 <link rel="stylesheet" type="text/css" href="css/about.css">
 <link rel="stylesheet" type="text/css" href="css/about1.css">
-<link rel="stylesheet" type="text/css" href="css/translator.css"> 
+<link rel="stylesheet" type="text/css" href="css/translator.css">
 </head>
  <body data-gr-c-s-loaded=true cz-shortcut-listen=true style="background-color:#E1EBF4">
  <div id=app class=english>
@@ -30,7 +30,7 @@
 		}
 	</style>
 <a href="index.php" class="btn btn-danger closebtn" style="width:auto; margin-top: 20px;">
-    Back To Home    
+    Back To Home
 </a>
 
 <div class="gt-1">
@@ -51,17 +51,17 @@
 		<div data-v-006917c1><div data-v-dc78402e data-v-006917c1 class=component-AboutHero><div data-v-dc78402e class="bg-container bg-cn1" ><div data-v-dc78402e class=bg-masker><div data-v-436329e0 data-v-dc78402e class=component-image-pre-loader style=width:100vw;height:100%;position:absolute;top:0;left:0><div data-v-b329ee4c data-v-436329e0 tabindex=-1 class=resize-observer><object style=display:block;position:absolute;top:0;left:0;height:100%;width:100%;overflow:hidden;pointer-events:none;z-index:-1 aria-hidden=true tabindex=-1 type=text/html data="data:text/html,<html>" sandbox></object></div> <div data-v-436329e0 class=pre-loader-overlay style=display:none></div> <img data-v-436329e0 src="images/background-tire.jpg" class="contain top-left" style="object-fit:contain;object-position:0 0;background-position:0 0"> </div></div> <svg data-v-dc78402e preserveAspectRatio="xMinYMid slice" viewBox="0 0 4011 1024" class=hero-mask><defs><path id=a d="M0 0h4011v1024H0z" class=path-1></path></defs><g fill=none fill-rule=evenodd><mask id=b fill=#fff><use xlink:href=#a></use></mask><g mask=url(#b)><g transform="translate(-3080 -2428)"><circle cx=2239.5 cy=2239.5 r=1739.5 stroke=#E1EBF4 stroke-width=2000 class=circle style=stroke-dasharray:1e-05px,10940.6px;stroke-dashoffset:-8197.95></circle><circle cx=2239.5 cy=2239.5 r=1739.5 stroke=#FFF stroke-width=2000 class="circle-white path" style=stroke-dasharray:1e-05px,10940.6px;stroke-dashoffset:-8197.95></circle><path fill=#E1EBF4 d="M3080 2428h4011v1024H3080V2428zm-840.5 2051c-601.597 0-1166.613-238.982-1583.566-655.934C238.982 3406.113 0 2841.097 0 2239.5S238.982 1072.887 655.934 655.934C1072.887 238.982 1637.903 0 2239.5 0s1166.613 238.982 1583.566 655.934C4240.018 1072.887 4479 1637.903 4479 2239.5s-238.982 1166.613-655.934 1583.566C3406.113 4240.018 2841.097 4479 2239.5 4479zm0-1000c333.654 0 645.183-131.766 876.459-363.041C3347.234 2884.683 3479 2573.154 3479 2239.5s-131.766-645.183-363.041-876.459C2884.683 1131.766 2573.154 1000 2239.5 1000s-645.183 131.766-876.459 363.041C1131.766 1594.317 1000 1905.846 1000 2239.5s131.766 645.183 363.041 876.459C1594.317 3347.234 1905.846 3479 2239.5 3479z" class=path-2></path></g></g></g></svg></div> <div data-v-dc78402e class=common-center-container><div data-v-dc78402e class=titles-container>
  		<div data-v-dc78402e class=line-one>
 		<div class=line style=display:block;text-align:right;position:relative;transform:matrix(1,0,0,1,0,0)> <!--Arizona--></div>
-		</div> 
+		</div>
 		<div data-v-dc78402e class=line-two>
 			<div class=line style=display:block;text-align:right;position:relative>
 				<div class=line style=display:block;text-align:right;position:relative;transform:matrix(1,0,0,1,0,0)>
-					Arizona <br/> premium tire 
+					Arizona <br/> premium tire
 				</div>
 			</div>
 			<div class=line style=display:block;text-align:right;position:relative>
 				<div class=line style=display:block;text-align:right;position:relative;transform:matrix(1,0,0,1,0,0)>distributor</div>
 			</div>
-		</div> 
+		</div>
  	<h3 data-v-dc78402e class=line-three></h3>
  </div>
 </div>
@@ -72,7 +72,7 @@
            <div data-v-236900ea data-v-006917c1 class=component-AboutNews><svg data-v-236900ea  width=331 height=331 viewBox="0 0 331 331" class=donut-background style=transform:translate3d(0,22.4352%,0)><path fill=#A5A5A5 fill-rule=evenodd d="M165.5 331C74.097 331 0 256.903 0 165.5S74.097 0 165.5 0 331 74.097 331 165.5 256.903 331 165.5 331zm1-78c47.773 0 86.5-38.727 86.5-86.5S214.273 80 166.5 80 80 118.727 80 166.5s38.727 86.5 86.5 86.5z"></path></svg> <div data-v-236900ea class=common-center-container>
  	  <div class="row">
  	 	<div class="col-md-6">
- 	 			<h3 data-v-44c19e12 class=heading style="color:#0015f9">COMPANY HISTORY</h3> 
+ 	 			<h3 data-v-44c19e12 class=heading style="color:#0015f9">COMPANY HISTORY</h3>
 		 		<div data-v-44c19e12 class=subheading style="color:black;text-align:justify;" >
 		 			<p>Wholesale Tire Distributor (WTD) structured truly by the GOD. WTD invented out with true and sincere efforts and intentions. This company truly works under the religious policies. Mostly companies make profit for their own growth, but WTD is the first True Business Partner, who help to the customers and selling all the product with very low profit margin so that its customer can also make good enough money for their growth. </p>
 
@@ -81,7 +81,13 @@
 		 		</div>
 		 	</div>
  	 	<div class="col-md-6">
- 	 	 <img src="https://www.motoringresearch.com/wp-content/uploads/2014/07/F1-13-inch-wheel-1.jpg" style="width:100%;">
+		  <div style="
+				margin-top: 60px;
+				height: 500px;
+				background-image: url(&quot;https://images.unsplash.com/photo-1444947295498-07f60c19a4ff?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;w=1000&amp;q=80&quot;);
+				background-position: center;
+				background-origin: initial;
+				" https:="" cdntdreditorials.azureedge.net="" cache="" e=""></div>
  	 	</div>
  	 </div>
  	 <div class="row">
@@ -139,9 +145,9 @@
   		 <img src="images/13.jpg" style="width:215px;color: red;">
   	     <div align="center" >
   	     	<div data-v-1066ae32 class=content >
- 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div> 
- 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div> 
- 							<div data-v-1066ae32 class=date></div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div>
+ 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div>
+ 							<div data-v-1066ae32 class=date></div>
   	     </div>
   	</div>
   </div>
@@ -149,10 +155,10 @@
   		<img src="images/14.jpg" style="width:215px;color: red">
   		<div align="center">
   			<div data-v-1066ae32 class=content>
- 							<div data-v-1066ae32 class=name style="color:red">Peter Reyher</div> 
- 							<div data-v-1066ae32 class=position style="color:red">VP, Safety and Business Support</div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Peter Reyher</div>
+ 							<div data-v-1066ae32 class=position style="color:red">VP, Safety and Business Support</div>
  							<div data-v-1066ae32 class=date>
- 								
+
  							</div>
   		</div>
   	</div>
@@ -161,9 +167,9 @@
   		<img src="images/13.jpg" style="width:215px;color: red">
   		<div align="center">
   				<div data-v-1066ae32 class=content>
- 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div> 
- 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div> 
- 							<div data-v-1066ae32 class=date></div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div>
+ 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div>
+ 							<div data-v-1066ae32 class=date></div>
   	     </div>
   		</div>
   	</div>
@@ -173,9 +179,9 @@
   		 <img src="images/13.jpg" style="width:215px;color: red">
   	     <div align="center">
   	     	<div data-v-1066ae32 class=content>
- 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div> 
- 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div> 
- 							<div data-v-1066ae32 class=date></div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div>
+ 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div>
+ 							<div data-v-1066ae32 class=date></div>
   	     </div>
   	</div>
   </div>
@@ -183,10 +189,10 @@
   		<img src="images/14.jpg" style="width:215px;color:red">
   		<div align="center">
   			<div data-v-1066ae32 class=content>
- 							<div data-v-1066ae32 class=name style="color:red">Peter Reyher</div> 
- 							<div data-v-1066ae32 class=position style="color:red">VP, Safety and Business Support</div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Peter Reyher</div>
+ 							<div data-v-1066ae32 class=position style="color:red">VP, Safety and Business Support</div>
  							<div data-v-1066ae32 class=date>
- 								
+
  							</div>
   		</div>
   	</div>
@@ -195,9 +201,9 @@
   		<img src="images/13.jpg" style="width:215px; color:red">
   		<div align="center">
   				<div data-v-1066ae32 class=content>
- 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div> 
- 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div> 
- 							<div data-v-1066ae32 class=date></div> 
+ 							<div data-v-1066ae32 class=name style="color:red">Michelle Lozano</div>
+ 							<div data-v-1066ae32 class=position style="color:red">Warehouse Associate</div>
+ 							<div data-v-1066ae32 class=date></div>
   	     </div>
   		</div>
   	</div>
@@ -205,19 +211,19 @@
    <div data-v-68d9be01 data-v-006917c1 id=form-section class=component-FormSection style="background-color:#090930">
 		 	<div data-v-68d9be01 class=common-center-container style="box-shadow: 3px 3px 5px 5px rgba(0,0,0,0.75);">
                 <?php
-                if (!empty($msg)) {
-                    if ($msg == 'success') {
-                        echo '<div class="alert alert-success">
+if (!empty($msg)) {
+    if ($msg == 'success') {
+        echo '<div class="alert alert-success">
                       <strong>Success!</strong> Application Successfully Submited
                     </div>';
-                    }else{
-                        echo '<div class="alert alert-danger">
+    } else {
+        echo '<div class="alert alert-danger">
                       <strong>Oooops!</strong> Application Not send Please try again.
                     </div>';
-                    }
-                }
-                ?>
-		 		<h3 data-v-68d9be01 class="heading webkit-font">GET A CALL BACK</h3> 
+    }
+}
+?>
+		 		<h3 data-v-68d9be01 class="heading webkit-font">GET A CALL BACK</h3>
 		 		<!-- <h4 data-v-68d9be01 class="subheading webkit-font">Let’s grow your business together. Provide your details, and an WTD associate will contact you.</h4>  -->
 		 		<form action="submitform.php" method="post">
 		 			<div class="col-sm-6 center-block" style="float: none;">
@@ -233,39 +239,39 @@
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary btn-block" style="background: red;    border: red;    font-weight: bold;" >SUBMIT</button>
                         </div>
-		 			</div> 
+		 			</div>
 		 		</form>
 		 	</div>
-		 </div> 
+		 </div>
 
 
 </div>
 <!-- </div> -->
-<!--  
+<!--
 </div>
 </div> -->
 
  <div data-v-c08ed51e data-v-006917c1 class=component-AboutProfiles style=background-image:url(images/black-and.jpg)>
  	<div data-v-c08ed51e class=common-center-container>
- 	 
-		   
 
- 
+
+
+
 	</div>
 </div>
 <!-- ------------- our team start ----------------------- -->
 <style type="text/css">
 	.component-CareersPeople[data-v-53b7536e]{position:relative;width:100%;min-height:50vh;background-color:#0015f9;overflow:hidden}.component-CareersPeople .donut-background[data-v-53b7536e]{z-index:0;position:absolute;top:-169px;left:auto;right:-50%;width:50vw;height:50vw;min-width:700px;min-height:700px}.component-CareersPeople .donut-background[data-v-53b7536e] path{fill:#1280c3}.component-CareersPeople .heading[data-v-53b7536e],.component-CareersPeople .subheading[data-v-53b7536e]{color:#ffffff;margin-bottom:30px}.component-CareersPeople .employee-profiles-container[data-v-53b7536e]{border:1px solid #ffffff;border-width:1px 0 0 1px;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap;position:relative}.component-Profile[data-v-69390e04]{border:1px solid #ffffff;border-width:0 1px 1px 0;width:33.33333%}@media (min-width:728px) and (max-width:1024px){.component-Profile[data-v-69390e04]{width:50%}}@media (min-width:300px) and (max-width:727px){.component-Profile[data-v-69390e04]{width:50%}}.component-Profile .image[data-v-69390e04]{-webkit-filter:grayscale(1);filter:grayscale(1);position:relative;z-index:1}.component-Profile .overlay[data-v-69390e04]{content:' ';position:absolute;left:0;top:0;width:100%;height:100%;background:#001D34;z-index:2;opacity:0.4;mix-blend-mode:hard-light;transition:opacity 0.3s linear}.content-container[data-v-69390e04]{cursor:pointer;display:block;height:294px;position:relative}@media (min-width:300px) and (max-width:727px){.content-container[data-v-69390e04]{height:147px}}.image-container[data-v-69390e04]{bottom:48px;left:0;position:absolute;top:0;width:100%}.image-container .image[data-v-69390e04]{background-size:cover;background-position:50% 50%;background-repeat:no-repeat;height:100%;transition:-webkit-filter 0.5s;transition:filter 0.5s;transition:filter 0.5s,-webkit-filter 0.5s;width:100%}@media (min-width:300px) and (max-width:727px){.image-container[data-v-69390e04]{bottom:30px}}.titles-container[data-v-69390e04]{background-color:red;bottom:0;left:0;position:absolute;height:48px;text-align:center;width:100%;z-index:2;overflow:hidden}.titles-container .sliding-container[data-v-69390e04]{height:96px;transition:transform 0.3s cubic-bezier(0.455,0.03,0.515,0.955)}.titles-container .sliding-container .name-container[data-v-69390e04]{height:48px;text-align:center;line-height:48px;text-overflow:ellipsis;white-space:nowrap;width:100%;padding:0 35px}@media (min-width:300px) and (max-width:727px){.titles-container .sliding-container .name-container[data-v-69390e04]{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;-ms-flex-pack:center;justify-content:center;-ms-flex-align:start;align-items:flex-start;padding:0 5px}}.titles-container .sliding-container .name-container .name[data-v-69390e04]{font-size:15px;letter-spacing:0em;margin-right:-0em;line-height:1em;font-style:normal;font-weight:400;font-family:"Tw Cen MT W05 Bold",monospace;-webkit-font-variant-ligatures:no-common-ligatures;color:#ffffff}@media (min-width:300px) and (max-width:727px){.titles-container .sliding-container .name-container .name[data-v-69390e04]{font-size:13px}}.titles-container .sliding-container .name-container .position[data-v-69390e04]{font-size:15px;letter-spacing:0em;margin-right:-0em;line-height:1em;font-style:normal;font-weight:400;font-family:"LTC Twentieth Century W05 Rg",monospace;font-variant-ligatures:none;-webkit-font-variant-ligatures:no-common-ligatures;color:#ffffff;text-overflow:ellipsis}@media (min-width:300px) and (max-width:727px){.titles-container .sliding-container .name-container .position[data-v-69390e04]{font-size:13px}}.titles-container .sliding-container .roll-over-container[data-v-69390e04]{height:48px;display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center}.titles-container .sliding-container .roll-over-container .roll-over-text[data-v-69390e04]{font-size:18px;letter-spacing:0em;margin-right:-0em;line-height:1em;font-style:normal;font-weight:400;font-family:"LTC Twentieth Century W05 Rg",monospace;font-variant-ligatures:none;-webkit-font-variant-ligatures:no-common-ligatures;color:#ffffff}.titles-container .name[data-v-69390e04],.titles-container .position[data-v-69390e04]{display:inline-block;vertical-align:baseline}.titles-container .triangle[data-v-69390e04]{width:16px;height:19px;position:absolute;right:18px;top:50%;transform:translateY(-50%)}@media (min-width:300px) and (max-width:727px){.titles-container .triangle[data-v-69390e04]{display:none}}.component-ProfileOverlay[data-v-1066ae32]{background-color:#001D34;border:1px solid #ffffff;display:-ms-flexbox;display:flex;height:591px;left:-1px;opacity:0;pointer-events:none;position:absolute;top:-1px;width:100%;z-index:3}@media (min-width:300px) and (max-width:727px){.component-ProfileOverlay[data-v-1066ae32]{border-width:1px 0;left:0}}.image-container[data-v-1066ae32]{height:100%;position:relative;z-index:2}@media (min-width:300px) and (max-width:727px){.image-container[data-v-1066ae32]{display:none}}.image-container .image[data-v-1066ae32]{background-size:cover;background-position:50% 50%;background-repeat:no-repeat;height:100%;position:relative;width:100%}.content-container[data-v-1066ae32]{z-index:2}@media (min-width:728px) and (max-width:1024px){.content-container[data-v-1066ae32]{width:75%}}@media (min-width:728px) and (max-width:1024px) and (min-width:300px) and (max-width:727px){.content-container[data-v-1066ae32]{width:100%}}@media (min-width:1025px){.content-container[data-v-1066ae32]{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;-ms-flex-pack:center;justify-content:center;width:37.5%}}.content[data-v-1066ae32]{margin:11.11111%}.close-button[data-v-1066ae32]{position:absolute;top:23px;right:21px;width:23px;height:21px;cursor:pointer;z-index:4}.name[data-v-1066ae32]{font-size:28px;letter-spacing:0em;margin-right:-0em;line-height:1.2em;font-style:normal;font-weight:400;font-family:"Tw Cen MT W05 Bold",monospace;-webkit-font-variant-ligatures:no-common-ligatures;color:#fdfdfd}.position[data-v-1066ae32]{font-size:24.5px;letter-spacing:0em;margin-right:-0em;line-height:1.2em;font-style:normal;font-weight:400;font-family:"LTC Twentieth Century W05 Rg",monospace;font-variant-ligatures:none;-webkit-font-variant-ligatures:no-common-ligatures;color:#fdfdfd}.date[data-v-1066ae32]{font-size:24.5px;letter-spacing:0em;margin-right:-0em;line-height:1.2em;font-style:normal;font-weight:400;font-family:"LTC Twentieth Century W05 Rg",monospace;font-variant-ligatures:none;-webkit-font-variant-ligatures:no-common-ligatures;color:#0076be;margin-bottom:25px}.blurb[data-v-1066ae32]{-webkit-font-variant-ligatures:no-common-ligatures}.blurb .left[data-v-1066ae32]{position:absolute;top:-5px;left:-25px;width:20px;height:14px}.blurb .right[data-v-1066ae32]{width:20px;height:14px;margin-bottom:5px}.careers .image-container[data-v-1066ae32]{width:50%}@media (min-width:300px) and (max-width:727px){.careers .image-container[data-v-1066ae32]{display:none}}.careers .content-container[data-v-1066ae32]{position:relative;width:50%}@media (min-width:300px) and (max-width:727px){.careers .content-container[data-v-1066ae32]{width:100%}}.careers .content-container .blurb[data-v-1066ae32]{font-size:24px;letter-spacing:0em;margin-right:-0em;line-height:1.18em;font-style:normal;font-weight:400;font-family:"AvantGardeGothicITC W08 Book",monospace;-webkit-font-variant-ligatures:no-common-ligatures;color:#ffffff;position:relative}@media (min-width:300px) and (max-width:727px){.careers .content-container .blurb[data-v-1066ae32]{font-size:16px}}.component-Careers[data-v-0b5d6b6e]{position:relative;top:0;left:0;width:100vw;overflow-x:hidden}
 </style>
-              
-             
+
+
  			</div>
  		</div>
  	</div>
  </div>
 <!-- ------------- our team end ----------------------- -->
-<?php include 'footer.php'; ?>
- 
- 
- 
- 
+<?php include 'footer.php';?>
+
+
+
+
